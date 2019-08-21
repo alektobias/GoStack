ad5d5a9ea59eb8ab5c2142692a686d39de2b2ade
Alek Tobias <alektobias@gmail.com>
2019-08-21 19:26:58 -0300

    User authentication done.

--- modulo-02/src/routes.js
+++ modulo-02/src/routes.js
@@ -1,7 +1,14 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
-routes.get('/', (req, res) => {
-  return res.json({ hello: 'world!' });
-});
+
+routes
+	.route('/users')
+	.post(UserController.store)
+	.put(authMiddleware, UserController.update);
+
+routes.post('/sessions', SessionController.store);
 module.exports = routes;
