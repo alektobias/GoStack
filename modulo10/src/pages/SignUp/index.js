f086a603f6392e5645067562e04b10f3d4ec85db
Alek Tobias <alektobias@gmail.com>
2019-10-12 11:30:00 -0300

    GoBarber mobile

---
+++ modulo10/src/pages/SignUp/index.js
@@ -0,0 +1,59 @@
+import React, { useRef } from 'react';
+import { Image } from 'react-native';
+import Background from '~/components/Background';
+import logo from '~/assets/logo.png';
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignUp({ navigation }) {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  function handleSubmit() {}
+
+  return (
+    <Background>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autocCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome completo"
+            returnKeyType="next"
+            onSubmitEditing={() => emailRef.current.focus()}
+          />
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autocCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+          />
+
+          <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
+          <SignLink onPress={() => navigation.navigate('SignIn')}>
+            <SignLinkText>Já tenho conta</SignLinkText>
+          </SignLink>
+        </Form>
+      </Container>
+    </Background>
+  );
+}
