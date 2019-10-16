ce11319213ab941430aff641bcd20429e6de09e0
Alek Tobias <alektobias@gmail.com>
2019-10-16 14:23:03 -0300

    Reudx mobile

--- modulo10/src/pages/SignIn/index.js
+++ modulo10/src/pages/SignIn/index.js
@@ -1,5 +1,6 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Image } from 'react-native';
+import { useDispatch } from 'react-redux';
 import Background from '~/components/Background';
 import logo from '~/assets/logo.png';
 
@@ -11,10 +12,18 @@ import {
   SignLink,
   SignLinkText,
 } from './styles';
+import { signInRequest } from '~/store/modules/auth/actions';
 
 export default function SignIn({ navigation }) {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const passwordRef = useRef();
-  function handleSubmit() {}
+
+  function handleSubmit() {
+    dispatch(signInRequest(email, password));
+  }
+
   return (
     <Background>
       <Container>
@@ -28,6 +37,8 @@ export default function SignIn({ navigation }) {
             placeholder="Digite seu e-mail"
             returnKeyType="next"
             onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
           />
           <FormInput
             icon="lock-outline"
@@ -36,6 +47,8 @@ export default function SignIn({ navigation }) {
             ref={passwordRef}
             returnKeyType="send"
             onSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword}
           />
 
           <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
--- modulo10/src/pages/SignUp/index.js
+++ modulo10/src/pages/SignUp/index.js
@@ -1,4 +1,5 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { Image } from 'react-native';
 import Background from '~/components/Background';
 import logo from '~/assets/logo.png';
@@ -11,12 +12,21 @@ import {
   SignLink,
   SignLinkText,
 } from './styles';
+import { signUpRequest } from '~/store/modules/auth/actions';
 
 export default function SignUp({ navigation }) {
+  const dispatch = useDispatch();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const emailRef = useRef();
   const passwordRef = useRef();
 
-  function handleSubmit() {}
+  function handleSubmit() {
+    dispatch(signUpRequest(name, email, password));
+  }
 
   return (
     <Background>
@@ -30,6 +40,8 @@ export default function SignUp({ navigation }) {
             placeholder="Nome completo"
             returnKeyType="next"
             onSubmitEditing={() => emailRef.current.focus()}
+            value={name}
+            onChangeText={setEmail}
           />
           <FormInput
             icon="mail-outline"
@@ -39,6 +51,8 @@ export default function SignUp({ navigation }) {
             placeholder="Digite seu e-mail"
             returnKeyType="next"
             onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
           />
           <FormInput
             icon="lock-outline"
@@ -46,6 +60,8 @@ export default function SignUp({ navigation }) {
             placeholder="Sua senha secreta"
             returnKeyType="send"
             onSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword}
           />
 
           <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
